449d244ccdc401c6e932e8493422ad33e520bffd
Reinout van Rees <reinout@vanrees.org>
2011-03-21 23:08:43 +0100

    Copied over most of my old settings (mostly python-related)

---
+++ custom.el
@@ -0,0 +1,24 @@
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(blink-matching-paren t)
+ '(column-number-mode t)
+ '(fill-column 78)
+ '(find-grep-options " ")
+ '(grep-command "svngrep")
+ '(grep-find-command nil)
+ '(grep-highlight-matches t)
+ '(ido-everywhere t)
+ '(ido-ignore-buffers (quote ("\\` " "\\`.*\\*$")))
+ '(ido-mode (quote both) nil (ido))
+ '(py-pychecker-command "pychecker.sh")
+ '(py-pychecker-command-args (quote ("")))
+ '(python-check-command "pychecker.sh")
+ '(save-place t nil (saveplace))
+ '(text-mode-hook (quote (turn-on-auto-fill text-mode-hook-identify)))
+ '(uniquify-buffer-name-style (quote forward) nil (uniquify))
+ '(vc-follow-symlinks t)
+ '(whitespace-global-mode nil)
+ '(windmove-wrap-around t))
