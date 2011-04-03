d3f75a26dabe36b06c33dbd72e9e768e45df34f0
Reinout van Rees <reinout@vanrees.org>
2011-04-03 23:06:35 +0200

    Copied over old config stuff about python and js: jslint, pep8/pflakes, etc

--- custom.el
+++ custom.el
@@ -13,6 +13,7 @@
  '(ido-everywhere t)
  '(ido-ignore-buffers (quote ("\\` " "\\`.*\\*$")))
  '(ido-mode (quote both) nil (ido))
+ '(kill-whole-line t)
  '(py-pychecker-command "pychecker.sh")
  '(py-pychecker-command-args (quote ("")))
  '(python-check-command "pychecker.sh")
@@ -22,3 +23,9 @@
  '(vc-follow-symlinks t)
  '(whitespace-global-mode nil)
  '(windmove-wrap-around t))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ )
--- reinout.el
+++ reinout.el
@@ -1,5 +1,69 @@
 ;; Reinout-specific customizations to the base starter kit.
 
+;; faster syntax highlighting
+(setq jit-lock-stealth-time 0.01)
+
+;; Get dired to consider .pyc and .pyo files to be uninteresting
+(add-hook 'dired-load-hook
+    (lambda ()
+      (load "dired-x")
+     ))
+(add-hook 'dired-mode-hook
+    (lambda ()
+      (setq dired-omit-files-p t)
+      (setq dired-omit-files (concat dired-omit-files "\\|^\\..+$"))
+     ))
+(load "dired")
+(setq dired-omit-extensions (append '(".pyc" ".pyo" ".bak")
+				    dired-omit-extensions))
+
+;; Zap trailing whitespace
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
+
+;; Alt-arrow navigation between emacs windows.
+(windmove-default-keybindings 'meta)
+
+;; Reset js/json to use the simpler and nicer javascript-mode
+(add-to-list 'auto-mode-alist '("\\.js$" . javascript-mode))
+(add-to-list 'auto-mode-alist '("\\.json$" . javascript-mode))
+
+;; Python mode stuff
+(add-hook 'python-mode-hook '(lambda () (define-key python-mode-map "\C-m" 'newline-and-indent)))
+
+;; Enable pyflakes and flymake
+;; http://www.plope.com/Members/chrism/flymake-mode
+(when (load "flymake" t)
+  (defun flymake-pyflakes-init ()
+    (let* ((temp-file (flymake-init-create-temp-buffer-copy
+                       'flymake-create-temp-inplace))
+           (local-file (file-relative-name
+                        temp-file
+                        (file-name-directory buffer-file-name))))
+      (list "pyflakes" (list local-file))))
+  (add-to-list 'flymake-allowed-file-name-masks
+               '("\\.py\\'" flymake-pyflakes-init)))
+
+(add-hook 'find-file-hook 'flymake-find-file-hook)
+
+;; Enable the locally-downloaded doctest-mode.el
+(autoload 'doctest-mode "doctest-mode" "doctest mode" t)
+
+;; Override starter-kit's ctrl-x TAB override back.
+(global-set-key (kbd "C-x C-i") 'indent-rigidly)
+
+(define-prefix-command 'reinout-bindings-keymap)
+(define-key reinout-bindings-keymap (vector ?f) 'auto-fill-mode)
+(define-key reinout-bindings-keymap (vector ?j) 'jslint-thisfile)
+(define-key reinout-bindings-keymap (vector ?s) 'sort-lines)
+(global-set-key [(f5)] 'reinout-bindings-keymap)
+
+;; javascript lint
+(defun jslint-thisfile ()
+  (interactive)
+  (compile (format "jslint %s" (buffer-file-name))))
+(add-hook 'javascript-mode-hook
+          '(lambda ()
+             (local-set-key (kbd "C-c C-w") 'jslint-thisfile)))
 
 ;; Start emacs as a server so that clients can connect.
 (server-start)
